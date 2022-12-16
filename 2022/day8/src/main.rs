7e2d84491f54c1ec0899ffc779cb4d03c6ff803a
Laura-Eliise <laura-eliis@marrandi.ee>
2022-12-16 20:54:18 +0200

    setting up files so it's easier to get started on the tasks

--- 2022/day8/src/main.rs
+++ 2022/day8/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::read_to_string;
+
 fn main() {
-    println!("Hello, world!");
+    let data = read_file("./src/info.txt");
+
+    println!("Part 1 answer is {}.", part1(data.clone()));
+    println!("Part 2 answer is {}.", part2(data.clone()));
+}
+
+fn read_file(path: &str) -> String {
+    let file = read_to_string(path);
+    return match file {
+        Ok(t) => t,
+        Err(error) => panic!("Could not read the file: {:?}", error),
+    };
+}
+
+fn part1(data: String) -> u32 {
+    let ans: u32 = 0;
+
+    return ans
+}
+fn part2(data: String) -> u32 {
+    let ans: u32 = 0;
+
+    return ans
 }
